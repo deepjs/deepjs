9aa668bcedec2dd66f446463a187b924dec9318d
Gilles Coomans <gilles.coomans@gmail.com>
2013-03-03 13:59:20 +0100

    updates

---
+++ DOCS/apidocs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "DeepHandler",
+        "SynchHandler"
+    ],
+    "modules": [
+        "deep"
+    ],
+    "allModules": [
+        {
+            "displayName": "deep",
+            "name": "deep",
+            "description": "deep : just say : Powaaaaaa ;)"
+        }
+    ]
+} };
+});
