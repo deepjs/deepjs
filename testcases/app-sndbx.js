6720b8fc461b925efc2f3725f457c7fa941c877c
Gilles Coomans <gilles.coomans@gmail.com>
2013-08-14 14:27:11 +0200

    add sandbox html page

---
+++ testcases/app-sndbx.js
@@ -0,0 +1,32 @@
+/**
+ * @author Gilles Coomans <gilles.coomans@gmail.com>
+ */
+define(function(require){
+	console.flags = { 
+		"none":false
+		//, "layer-factory": true
+		//, "request-factory":true
+		, "form-controller":true
+		//, "view-controller":true
+		//,"list-controller":true
+		//,"list-item-controller":true
+	};
+	console.log("start app-sndbx");
+	deep = require("deep/deep");
+	//require("deep/deep-roles")(deep);
+	/*deep(deep.roles).up({
+		"user":{
+			name:"user",
+			backgrounds:[deep.Role],
+			stores:{
+				
+			}
+		}
+	})
+	.flatten();*/
+	var init = function()
+	{	
+		console.log("app-sndbx intialised");
+	}
+	return init;
+})
---
+++ testcases/main-sndbx.js
@@ -0,0 +1,18 @@
+// main.js : load all first dependencies
+
+
+console.flags = {}
+console.flog = function(flag, message)
+{
+	if(console.flags[flag])
+		console.log(flag+" : "+message);
+}
+
+require.config({
+	 baseUrl: "/js",
+    "packages": [{"name":"deep", "main":"deep.js"}]
+});
+require([ "app-sndbx.js", "deep-ui/plugin", "deep-swig/main","/js/swig/swig.pack.min.js"], function( app, deep ) {
+	// console.log("deep ? ", deep);
+  	app();
+});
