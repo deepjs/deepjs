f387d2ada38fdd25202ef08edf1aa9e765ec8c14
Philippe Delabye <phil@chemicalrules>
2013-09-20 11:37:08 +0200

    tutos LAOP + query based (WiP)

---
+++ DOCS/query-based-object-manipulation.js
@@ -0,0 +1,47 @@
+//-----------------Models
+var land = {
+	location : "pls override this value with your own location",
+	watering : function () {
+		console.log("Water is applied on all the land");
+	}
+};
+//-----------------Aspects
+var orchard = {
+	plants : [
+		{id:"appletree", growSpeed : 5},
+		{id:"kiwi", growSpeed : 10}
+	],
+	watering : deep.compose.after(function () {
+		console.log("We put some mulch after watering");
+	})
+};
+
+var kitchenGarden = {
+	plants : [
+		{id:"carrots", growSpeed : 2},
+		{id:"sunflower", growSpeed : 8}
+	],
+	watering : deep.compose.before(function () {
+		console.log("We protect seedlings before watering");
+	})
+};
+//-----------------Implement
+
+var myOwnLand = {
+	location : "Sao Francisco do Guaporé",
+	plants : [
+		{id:"grass", growSpeed : 3}
+	]
+};
+
+deep(myOwnLand).bottom(land).up(orchard,kitchenGarden).query("/plants/*").up({
+	size : 0,
+	grow : function () {
+		this.size += growSpeed;
+	},
+	prune : function (heightpruned) {
+		this.size -= heightpruned;
+	}
+});
+
+console.log("myOwnLand is : ", myOwnLand);
