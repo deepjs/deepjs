5bfe11023773e1a49512ca90f430a26a888c7df3
Gilles Coomans <gilles.coomans@gmail.com>
2013-10-20 22:52:54 +0200

    add fileName and linenum for all errors

--- deep-errors.js
+++ deep-errors.js
@@ -21,96 +21,108 @@ define(function(require)
 			},
 			PreconditionFail:function(msg, report, fileName, lineNum)
 			{
-				var error = new Error(msg);
+				var error = new Error(msg, fileName, lineNum);
 				error.status = 412;
 				error.report = report;
 				return error;
 			},
 			NotFound:function(msg, report, fileName, lineNum)
 			{
-				var error = new Error(msg);
+				var error = new Error(msg, fileName, lineNum);
 				error.status = 404;
 				error.report = report;
 				return error;
 			},
 			Forbidden:function(msg, report, fileName, lineNum)
 			{
-				var error = new Error(msg);
+				var error = new Error(msg, fileName, lineNum);
 				error.status = 403;
 				error.report = report;
 				return error;
 			},
 			Range:function(msg, report, fileName, lineNum)
 			{
-				var error = new Error(msg);
+				var error = new Error(msg, fileName, lineNum);
 				error.status = 416;
 				error.report = report;
 				return error;
 			},
 			Assertion:function(msg, report, fileName, lineNum)
 			{
-				var error = new Error("AssertionError : "+msg);
+				var error = new Error("AssertionError : "+msg, fileName, lineNum);
 				error.status = 412;
 				error.report = report;
 				return error;
 			},
 			Mode:function  (msg, report, fileName, lineNum) {
-				var error = new Error("ModeError : "+msg);
+				var error = new Error("ModeError : "+msg, fileName, lineNum);
 				error.status = 500;
 				error.report = report;
 				return error;
 			},
 			Watch:function  (msg, report, fileName, lineNum) {
-				var error = new Error("FileWatchError : "+msg);
+				var error = new Error("FileWatchError : "+msg, fileName, lineNum);
 				error.status = 500;
 				error.report = report;
 				return error;
 			},
 			Store:function  (msg, report, fileName, lineNum) {
-				var error = new Error("StoreError : "+msg);
+				var error = new Error("StoreError : "+msg, fileName, lineNum);
 				error.status = 400;
 				error.report = report;
 				return error;
 			},
 			Protocole:function  (msg, report, fileName, lineNum) {
-				var error = new Error("ProtocoleError : "+msg);
+				var error = new Error("ProtocoleError : "+msg, fileName, lineNum);
 				error.status = 400;
 				error.report = report;
 				return error;
 			},
 			OCM:function  (msg, report, fileName, lineNum) {
-				var error = new Error("OCMError : "+msg);
+				var error = new Error("OCMError : "+msg, fileName, lineNum);
 				error.status = 403;
 				error.report = report;
 				return error;
 			},
 			Post:function  (msg, report, fileName, lineNum) {
-				var error = new Error("PostError : "+msg);
+				var error = new Error("PostError : "+msg, fileName, lineNum);
 				error.status = 400;
 				error.report = report;
 				return error;
 			},
 			Put:function  (msg, report, fileName, lineNum) {
-				var error = new Error("PutError : "+msg);
+				var error = new Error("PutError : "+msg, fileName, lineNum);
 				error.status = 400;
 				error.report = report;
 				return error;
 			},
 			Patch:function  (msg, report, fileName, lineNum) {
-				var error = new Error("PatchError : "+msg);
+				var error = new Error("PatchError : "+msg, fileName, lineNum);
 				error.status = 400;
 				error.report = report;
 				return error;
 			},
 			Delete:function  (msg, report, fileName, lineNum) {
-				var error = new Error("DeleteError : "+msg);
+				var error = new Error("DeleteError : "+msg, fileName, lineNum);
 				error.status = 400;
 				error.report = report;
 				return error;
 			},
 			RPC:function  (msg, report, fileName, lineNum) {
-				var error = new Error("RPCError : "+msg);
+				var error = new Error("RPCError : "+msg, fileName, lineNum);
 				error.status = 400;
+				error.report = report;
+				return error;
+			},
+			ChainEnded:function  (msg, report, fileName, lineNum) {
+				var error = new Error("ChainEndedError : "+msg, fileName, lineNum);
+				error.status = 1001;
+				error.report = report;
+				return error;
+			},
+			ChainListened:function  (msg, report, fileName, lineNum) {
+				var error = new Error("ChainListenedError : "+msg, fileName, lineNum);
+				error.status = 1002;
 				error.report = report;
 				return error;
 			}
