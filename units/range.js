6c566e8e9f17be384a04253a133cd65b79a5b4b9
Gilles Coomans <gilles.coomans@gmail.com>
2013-11-01 13:34:05 +0100

    work on range testcases + add check on rangeObject

--- units/range.js
+++ units/range.js
@@ -11,15 +11,47 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
         title:"deep/units/range",
         stopOnError:false,
         tests : {
-            "1-4":function(){
+            "1:4":function(){
                 return deep([0,1,2,3,4,5])
                 .range(1,4)
                 .valuesEqual([1,2,3,4]);
             },
-            "3-5":function(){
-                return deep([0,1,2,3,4,5])
-                .range(3,5)
-                .valuesEqual([3,4,5]);
+            "2:4":function(){
+                return deep(["hello","hell","heaven","helicopter","heat","here"])
+                .range(2, 4)
+                .equal( {
+                 "_deep_range_": true,
+                 "total": 6,
+                 "count": 3,
+                 "results": [
+                  "heaven",
+                  "helicopter",
+                  "heat"
+                 ],
+                 "start": 2,
+                 "end": 4,
+                 "hasNext": true,
+                 "hasPrevious": true
+                })
+                .valuesEqual(["heaven","helicopter","heat"]);
+            },
+            "2:5/?=match=(hel)":function(){
+                return deep(["hello","hell","heaven","helicopter","heat","here"])
+                .range(2, 5, "./?=match=(hel)")
+                .equal(
+                 {
+                 "_deep_range_": true,
+                 "total": 3,
+                 "count": 1,
+                 "results": [
+                  "helicopter"
+                 ],
+                 "start": 2,
+                 "end": 5,
+                 "hasNext": false,
+                 "hasPrevious": true
+                })
+                .valuesEqual(["helicopter"]);
             }
         }
     };
