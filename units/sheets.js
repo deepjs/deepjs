ba27c15fee02a3c6ac0e33fc1d3a2c15fca597d0
Gilles Coomans <gilles.coomans@gmail.com>
2013-11-01 18:24:34 +0100

    extracted sheets stuff + sheets one step beyond : welcome deep.sheeters

--- units/sheets.js
+++ units/sheets.js
@@ -8,7 +8,7 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
 
 
     var unit = {
-        title:"Unit title",
+        title:"deepjs/units/sheets",
         stopOnError:false,
         setup:function(){},
         tests : {
@@ -37,6 +37,78 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
 
 				deep.utils.sheet(sheet, obj);
 				return deep(obj).equal({"array":["from bottom","base entry","from up"],"a":{"test":123,"other":true},"hello":"world"});
+			},
+			transform:function(){
+				var sheet = {
+					"dq.transform::./*":function(input){
+						return "e"+input;
+					}
+				};
+				var obj = [1,2,3,4,5];
+
+				return deep(obj).sheet(sheet)
+				.equal({
+					"dq.transform::./*": [
+						"e1",
+						"e2",
+						"e3",
+						"e4",
+						"e5"
+					]
+				})
+				.valuesEqual(["e1", "e2", "e3", "e4", "e5"]);
+			},
+			through:function(){
+				var sheet = {
+					"dq.through::./*":function(input){
+						return "e"+input;
+					}
+				};
+				var obj = [1,2,3,4,5];
+
+				return deep(obj).sheet(sheet)
+				.equal({
+					"dq.through::./*": [
+						"e1",
+						"e2",
+						"e3",
+						"e4",
+						"e5"
+					]
+				})
+				.valuesEqual([1,2,3,4,5]);
+			},
+			sheeter1:function(){
+				var sheet = {
+				"dq.sheeter::./*":  deep.sheeter
+									.up({ fromUp:"tools" })
+									.bottom({ fromBottom:"hello" })
+				};
+
+				var obj = {
+					a:{
+						base:"deepjs"
+					}
+				};
+
+				return deep(obj)
+				.sheet(sheet)
+				.equal({
+					"dq.sheeter::./*": [
+						{
+						"fromBottom": "hello",
+						"base": "deepjs",
+						"fromUp": "tools"
+						}
+					]
+				})
+				.valuesEqual( {
+					"a": {
+						"fromBottom": "hello",
+						"base": "deepjs",
+						"fromUp": "tools"
+					}
+				});
 			}
         }
     };
