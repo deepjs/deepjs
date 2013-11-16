c9cfd1445f1d9c5df6731d8528abdd558f0111a1
Gilles Coomans <gilles.coomans@gmail.com>
2013-11-16 11:04:54 +0100

    add ocm test cases

--- units/ocm.js
+++ units/ocm.js
@@ -11,7 +11,54 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
         title:"deep/units/ocm",
         stopOnError:false,
         tests : {
-           
+			base:function(){
+				var myManager = deep.ocm({
+					mode1:{
+						test:1
+					},
+					mode2:{
+						test:2,
+						title:"hello world"
+					},
+					mode3:{
+						backgrounds:["this::../mode2"],
+						description:"mode 3 description"
+					}
+				});
+				myManager.flatten(); // seek and apply backgrounds
+				return deep.when([myManager("mode1"),myManager("mode2"), myManager("mode3")]).equal(
+					[
+						{ test:1 },
+						{ test:2, title:"hello world"},
+						{ test:2, title:"hello world", description:"mode 3 description"}
+					]
+				);
+           },
+           currentMode:function(){
+				var myManager = deep.ocm({
+					mode1:{
+						title:"should not see this"
+					},
+					mode2:{
+						title:"hello world"
+					}
+				});
+				myManager.mode("mode2");
+				return deep.when(myManager())
+				.equal({ title:"hello world" });
+			},
+			modeCollection:function(){
+				var myManager = deep.ocm({
+					mode1:{
+						test:1
+					},
+					mode2:{
+						title:"hello world"
+					}
+				});
+				return deep.when([myManager("mode1", "mode2"), myManager("mode2", "mode1")])
+				.equal([ { test:1, title:"hello world"}, { title:"hello world", test:1 }]);
+			}
         }
     };
 
