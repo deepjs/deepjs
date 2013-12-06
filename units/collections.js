e81fb4389df35ce7427357b8c88b02782a7d2953
Gilles Coomans <gilles.coomans@gmail.com>
2013-12-06 15:24:37 +0100

    add and clean collections testcases

--- units/collections.js
+++ units/collections.js
@@ -19,21 +19,31 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
                 var store = deep.store.Collection.create(null, [{ id:"u1", email:"gilles.coomans@gmail.com" }]);
                 return deep.store(store)
                 .get("u1")
-                .equal({ id:"u1", email:"gilles.coomans@gmail.com" });
+                .equal({ id:"u1", email:"gilles.coomans@gmail.com" })
+                .valuesEqual({ id:"u1", email:"gilles.coomans@gmail.com" });
+            },
+            query:function(){
+                var store = deep.store.Collection.create(null, [{ id:"u1", email:"gilles.coomans@gmail.com" }]);
+                return deep.store(store)
+                .get("?id=u1")
+                .equal([{ id:"u1", email:"gilles.coomans@gmail.com" }])
+                .valuesEqual([{ id:"u1", email:"gilles.coomans@gmail.com" }]);
             },
             post:function(){
                 var store = deep.store.Collection.create(null, []);
                 return deep.store(store)
                 .post({ id:"u1", email:"gilles.coomans@gmail.com" })
                 .equal({ id:"u1", email:"gilles.coomans@gmail.com" })
+                .valuesEqual({ id:"u1", email:"gilles.coomans@gmail.com" })
                 .get("u1")
                 .equal({ id:"u1", email:"gilles.coomans@gmail.com" });
             },
             put:function(){
-                var store = deep.store.Collection.create(null, [{ id:"u1", email:"gilles@gmail.com" }]);
+                var store = deep.store.Collection.create(null, [{ id:"u1", email:"toto@gmail.com" }]);
                 return deep.store(store)
                 .put({ id:"u1", email:"gilles@gmail.com" })
                 .equal({ id:"u1", email:"gilles@gmail.com" })
+                .valuesEqual({ id:"u1", email:"gilles@gmail.com" })
                 .get("u1")
                 .equal({ id:"u1", email:"gilles@gmail.com" });
             },
@@ -42,10 +52,191 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
                 return deep.store(store)
                 .patch({ email:"gilles@gmail.com" }, { id:"u1"})
                 .equal({ id:"u1", email:"gilles@gmail.com" })
+                .valuesEqual({ id:"u1", email:"gilles@gmail.com" })
                 .get("u1")
                 .equal({ id:"u1", email:"gilles@gmail.com" });
             },
-           "privateGet":function(){
+            del:function(){
+                var store = deep.store.Collection.create(null, [{ id:"u1", email:"gilles@gmail.com" }]);
+                return deep.store(store)
+                .del('u1')
+                .equal(true)
+                .valuesEqual(true)
+                .get("u1")
+                .fail(function(error){
+                     if(error && error.status == 404)
+                        return "lolipop";
+                })
+                .equal("lolipop");
+            },
+            range:function(){
+                var store = deep.store.Collection.create(null, [
+                    { id:"u1", count:1 },
+                    { id:"u2", count:2 },
+                    { id:"u3", count:3 },
+                    { id:"u4", count:4 },
+                    { id:"u5", count:5 },
+                    { id:"u6", count:6 }
+                ]);
+
+                return deep.store(store)
+                .range(2,4)
+                .equal({ _deep_range_: true,
+                  total: 6,
+                  count: 3,
+                  results:
+                    [
+                        { id:"u3", count:3 },
+                        { id:"u4", count:4 },
+                        { id:"u5", count:5 }
+                    ],
+                  start: 2,
+                  end: 4,
+                  hasNext: true,
+                  hasPrevious: true,
+                  query: '&limit(3,2)'
+                })
+                .valuesEqual([
+                    { id:"u3", count:3 },
+                    { id:"u4", count:4 },
+                    { id:"u5", count:5 }
+                ]);
+            },
+            rangeWithQuery:function(){
+                var store = deep.store.Collection.create(null, [
+                    { id:"u1", count:1 },
+                    { id:"u2", count:2 },
+                    { id:"u3", count:3 },
+                    { id:"u4", count:4 },
+                    { id:"u5", count:5 },
+                    { id:"u6", count:6 }
+                ]);
+
+                return deep.store(store)
+                .range(2,4, '?count=ge=3')
+                .equal({ _deep_range_: true,
+                  total: 4,
+                  count: 2,
+                  results:
+                    [
+                        { id:"u5", count:5 },
+                        { id:"u6", count:6 }
+                    ],
+                  start: 2,
+                  end: 3,
+                  hasNext: false,
+                  hasPrevious: true,
+                  query: "?count=ge=3&limit(2,2)"
+                })
+                .valuesEqual([
+                    { id:"u5", count:5 },
+                    { id:"u6", count:6 }
+                ]);
+            },
+            rpc:function(){
+                var checker = {};
+                var store = deep.store.Collection.create(null, [{ id:"u1", base:"was there before"}], null, {
+                    methods:{
+                        testrpc:function(handler, arg1, arg2)
+                        {
+                            checker.throughRPC = true;
+                            checker.args = [arg1, arg2];
+                            checker.base = this.base;
+                            this.decorated = "hello rpc";
+                            return handler.save();
+                        }
+                    }
+                });
+                return deep.store(store)
+                .rpc("testrpc", [1456, "world"], "u1")
+                .equal({ id:"u1", base:"was there before", decorated:"hello rpc" })
+                .valuesEqual({ id:"u1", base:"was there before", decorated:"hello rpc" })
+                .get("u1")
+                .equal({ id:"u1", base:"was there before", decorated:"hello rpc" })
+                .deep(checker)
+                .equal({
+                    throughRPC:true,
+                    args:[1456, "world"],
+                    base:"was there before"
+                });
+            },
+            rpcCopy:function(){
+                var checker = {};
+                var store = deep.store.Collection.create(null, [{ id:"u1", base:"was there before"}], null, {
+                    methods:{
+                        testrpc:function(handler, arg1, arg2)
+                        {
+                            checker.throughRPC = true;
+                            checker.args = [arg1, arg2];
+                            checker.base = this.base;
+                            this.decorated = "hello rpc";
+                            return "lolipop";
+                        }
+                    }
+                });
+                return deep.store(store)
+                .rpc("testrpc", [1456, "world"], "u1")
+                .equal("lolipop")
+                .valuesEqual("lolipop")
+                .get("u1")
+                .equal({ id:"u1", base:"was there before" })
+                .deep(checker)
+                .equal({
+                    throughRPC:true,
+                    args:[1456, "world"],
+                    base:"was there before"
+                });
+            },
+            getCopy:function(){
+                var store = deep.store.Collection.create(null, [{ id:"u1", email:"gilles.coomans@gmail.com" }]);
+                return deep.store(store)
+                .get("u1")
+                .done(function(s){
+                    s.hello = "world";
+                })
+                .equal({ id:"u1", email:"gilles.coomans@gmail.com", hello:"world" })
+                .get("u1")
+                .equal({ id:"u1", email:"gilles.coomans@gmail.com"});
+            },
+            queryCopy:function(){
+                var store = deep.store.Collection.create(null, [{ id:"u1", email:"gilles.coomans@gmail.com" }]);
+                return deep.store(store)
+                .get("?id=u1")
+                .done(function(s){
+                    s[0].hello = "world";
+                })
+                .equal([{ id:"u1", email:"gilles.coomans@gmail.com", hello:"world" }])
+                .get("?id=u1")
+                .equal([{ id:"u1", email:"gilles.coomans@gmail.com"}]);
+            },
+            rangeCopy:function(){
+                var store = deep.store.Collection.create(null, [
+                    { id:"u1", count:1 },
+                    { id:"u2", count:2 },
+                    { id:"u3", count:3 },
+                    { id:"u4", count:4 },
+                    { id:"u5", count:5 },
+                    { id:"u6", count:6 }
+                ]);
+
+                return deep.store(store)
+                .range(2,4)
+                .done(function(range){
+                    var res = range.results;
+                    res[0].hello = "world";
+                    return res;
+                })
+                .equal(
+                    [
+                        { id:"u3", count:3, hello:"world" },
+                        { id:"u4", count:4 },
+                        { id:"u5", count:5 }
+                    ]
+                )
+                .get("u3")
+                .equal({ id:"u3", count:3 });
+            },
+            privateGet:function(){
                 var store = deep.store.Collection.create(null, [{ id:"u1", email:"gilles.coomans@gmail.com", password:"test"}], {
                     properties:{
                         password:{ type:"string", "private":true }
@@ -53,9 +244,10 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
                 });
                 return deep.store(store)
                 .get("u1")
-                .equal({ id:"u1", email:"gilles.coomans@gmail.com" });
-           },
-           "privateQuery":function(){
+                .equal({ id:"u1", email:"gilles.coomans@gmail.com" })
+                .valuesEqual({ id:"u1", email:"gilles.coomans@gmail.com" });
+            },
+            privateQuery:function(){
                 var store = deep.store.Collection.create(null, [{ id:"u1", email:"gilles.coomans@gmail.com", password:"test"}], {
                     properties:{
                         password:{ type:"string", "private":true }
@@ -64,8 +256,8 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
                 return deep.store(store)
                 .get("?id=u1")
                 .equal([{ id:"u1", email:"gilles.coomans@gmail.com" }]);
-           },
-           "privatePost":function(){
+            },
+            privatePost:function(){
                 var store = deep.store.Collection.create(null, [], {
                     properties:{
                         password:{ type:"string", "private":true }
@@ -75,7 +267,7 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
                 .post({ id:"u2", email:"john.doe@gmail.com", password:"test"})
                 .equal({ id:"u2", email:"john.doe@gmail.com" });
            },
-           "privatePatch":function(){
+           privatePatch:function(){
                 var store = deep.store.Collection.create(null, [{ id:"u1", email:"gilles.coomans@gmail.com", password:"test"}], {
                     properties:{
                         password:{ type:"string", "private":true }
@@ -85,7 +277,7 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
                 .patch({ id:"u1", email:"john.doe@gmail.com" })
                 .equal({ id:"u1", email:"john.doe@gmail.com" });
            },
-           "readOnly":function(){
+           readOnly:function(){
                 var store = deep.store.Collection.create(null, [{ id:"i1", title:"hello" }], {
                     properties:{
                         title:{ readOnly:true, type:"string" }
@@ -99,12 +291,9 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
                 })
                 .equal("lolipop");
            },
-           "ownerPatchFail":function(){
+           ownerPatchFail:function(){
                 var store = deep.store.Collection.create(null, [{ id:"i1", label:"weee", userID:"u1" }], {
-                    ownerRestriction:true,
-                    properties:{
-                        label:{ type:"string" }
-                    }
+                    ownerRestriction:true
                 });
                 return deep.store(store)
                 .patch({ id:"i1", label:"yesssss" })
@@ -116,10 +305,7 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
             },
             "ownerPatchOk":function(){
                 var store = deep.store.Collection.create(null, [{ id:"i1", label:"weee", userID:"u1" }], {
-                    ownerRestriction:true,
-                    properties:{
-                        label:{ type:"string" }
-                    }
+                    ownerRestriction:true
                 });
                 deep.context.session = {
                     remoteUser:{ id:"u1" }
@@ -131,10 +317,7 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
             },
             "ownerPutFail":function(){
                 var store = deep.store.Collection.create(null, [{ id:"i1", label:"weee", userID:"u1" }], {
-                    ownerRestriction:true,
-                    properties:{
-                        label:{ type:"string" }
-                    }
+                    ownerRestriction:true
                 });
                 deep.context.session = {
                     remoteUser:{ id:"u2" }
@@ -149,10 +332,7 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
             },
             "ownerPutOk":function(){
                 var store = deep.store.Collection.create(null, [{ id:"i1", label:"weee", userID:"u1" }], {
-                    ownerRestriction:true,
-                    properties:{
-                        label:{ type:"string" }
-                    }
+                    ownerRestriction:true
                 });
                 deep.context.session = {
                     remoteUser:{ id:"u1" }
@@ -164,10 +344,7 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
             },
             "ownerDelFail":function(){
                 var store = deep.store.Collection.create(null, [{ id:"i1", label:"weee", userID:"u1" }], {
-                    ownerRestriction:true,
-                    properties:{
-                        label:{ type:"string" }
-                    }
+                    ownerRestriction:true
                 });
                 deep.context.session = {
                     remoteUser:{ id:"u2" }
@@ -178,15 +355,11 @@ define(["require","../deep", "../deep-unit"], function (require, deep, Unit) {
             },
             "ownerDelOk":function(){
                 var store = deep.store.Collection.create(null, [{ id:"i1", label:"weee", userID:"u1" }], {
-                    ownerRestriction:true,
-                    properties:{
-                        label:{ type:"string" }
-                    }
+                    ownerRestriction:true
                 });
                 deep.context.session = {
                     remoteUser:{ id:"u1" }
                 };
-
                 return deep.store(store)
                 .del("i1")
                 .equal(true);
