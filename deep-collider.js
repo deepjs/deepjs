9df497166c48126062c1b54ac6a0c06eac70a06d
Gilles Coomans <gilles.coomans@gmail.com>
2014-01-03 19:15:45 +0100

    completly rewrite Colliders. chainable, _deep_merger_

--- deep-collider.js
+++ deep-collider.js
@@ -22,9 +22,9 @@ if (typeof define !== 'function') {
  *
  * - add asynch collision (up, bottom) mode
  * 
- * 	- gives fine wrapper for custom behaviour (need chaining capabilities)
- * 		- ifCollision : don't keep collider if no collision
- * 		- dontKeep : do not keep collider (it will only be used to fire something as a test)
+ * - gives fine wrapper for custom behaviour (need chaining capabilities)
+ * - ifCollision : don't keep collider if no collision
+ * - dontKeep : do not keep collider (it will only be used to fire something as a test)
  */
 
 define(function(require)
@@ -35,9 +35,166 @@ define(function(require)
 	 * @class collider
 	 * @static
 	 */
-	 return function(deep){
+	return function(deep){
 
-		return {
+		deep.colliders = {};
+
+		deep.Collider = function(fn){
+			this._collision = fn;
+			this._deep_collider_ = true;
+			this._deep_merger_ = true;
+		};
+
+		deep.Collider.prototype = {
+			wrapIn : function(fn){
+				var coll = this._collision;
+				return function(input){
+					return fn(coll(input));
+				};
+			},
+			clone : function (){
+				//console.log("this on colliders clone ? ", this);
+				return deep.utils.copy(this, true);
+			},
+			up : function(obj){
+				if(!obj._deep_collider_)
+					return obj;
+				this._collision = this.wrapIn(obj._collision);
+				return this;
+			},
+			bottom : function(obj){
+				if(!obj._deep_collider_)
+					return this._collision(obj);
+				this._collision = obj.wrapIn(this._collision);
+				return this;
+			}
+		};
+
+		deep.Collider.add = function(name, fn){
+			var handler = function(){
+				var args = Array.prototype.slice.apply(arguments);
+				var h = function(input){
+					args.unshift(input);
+					return fn.apply({}, args);
+				};
+				if(!this._deep_collider_)
+					return new deep.Collider(h);
+				this._collision = this.wrapIn(h);
+				return this;
+			};
+			deep.colliders[name] = deep.Collider.prototype[name] = handler;
+			return deep.Collider;
+		};
+
+		/**
+		 * replace
+		 *
+		 * @example 
+		 * var a = {
+		 * test:deep.colliders.replace(12)
+		 * };
+		 * 
+		 */
+		deep.Collider.add("replace", function(input, by){ return by; });
+
+
+		/**
+		 * replace
+		 *
+		 * @example 
+		 * var a = {
+		 * test:deep.colliders.copyTo("my.path" ,myObject)
+		 * };
+		 * 
+		 */
+		deep.Collider.add("copyTo", function(input, path, object){ 
+			deep.utils.setValueByPath(object, path, input);
+			return input;
+		});
+
+
+		/**
+		 * log
+		 *
+		 * @example 
+		 * var a = {
+		 * 	test:deep.colliders.log("my title")
+		 * };
+		 * 
+		 */
+		deep.Collider.add("log", function(input, title){
+			console.log("deep.collider.log : ", title, ":", input);
+			return input;
+		});
+
+
+		/**
+		 * validate
+		 *
+		 * @example 
+		 * var a = {
+		 * test:deep.colliders.validate( mySchema )
+		 * };
+		 * 
+		 */
+		deep.Collider.add("validate", function(input, schema){
+			var report = deep.validate(input, schema);
+			if(!report.valid)
+				throw deep.errors.PreconditionFail(report);
+			return input;
+		});
+
+
+		/**
+		 * array.insertAt
+		 *
+		 * @example 
+		 * var a = {
+		 * test:deep.colliders["array.insertAt"]( what, index )
+		 * };
+		 * 
+		 */
+		deep.Collider.add("array.insertAt", function(input, what, index){
+			if(!(input instanceof Array))
+				throw deep.errors.Internal("colliders.array.insertAt couldn't be applied : target is not an array.");
+			var args = [index, 0].concat(what);
+			input.splice.apply(input, args);
+			return input;
+		});
+
+		/**
+		 * array.removeAt
+		 *
+		 * @example 
+		 * var a = {
+		 * test:deep.colliders["array.removeAt"]( index )
+		 * };
+		 * 
+		 */
+		deep.Collider.add("array.removeAt", function(input, index){
+			if(!(input instanceof Array))
+				throw deep.errors.Internal("colliders.array.insertAt couldn't be applied : target is not an array.");
+			input.splice(index);
+			return input;
+		});
+
+
+		/**
+		 * transform
+		 *
+		 * @example 
+		 * var a = {
+		 * test:deep.colliders.transform( function(input){ return input+2; } )
+		 * };
+		 * 
+		 */
+		deep.Collider.add("transform", function(input, fn){
+			return fn(input);
+		});
+
+		return deep.colliders;
+
+		var BACKUP = {
 			/**
 			 * wrap wrapped function with wrapper function. wrapped will receive ollided value as argument.
 			 * @method wrap
