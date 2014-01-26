bfa60f5b9435a85576f320ee392eb58c1454d158
Gilles Coomans <gilles.coomans@gmail.com>
2014-01-27 00:56:28 +0100

    add more interpret test cases

--- units/interpret.js
+++ units/interpret.js
@@ -27,14 +27,19 @@ define(["require","../deep", "../lib/unit"], function (require, deep, Unit) {
                 .equal("mystring 12");
             },
             c:function(){
-                return deep("mystring { id | 'hello world' } after")
+                return deep("mystring { id | 'hello' } after")
                 .interpret({id:undefined})
-                .equal("mystring hello world after");
+                .equal("mystring hello after");
             },
             d:function(){
                 return deep("mystring { id | '0' } after")
                 .interpret({id:12})
                 .equal("mystring 12 after");
+            },
+            e:function(){
+                return deep("mystring { 'hello world' } after")
+                .interpret({id:12})
+                .equal("mystring hello world after");
             }
         }
     };
