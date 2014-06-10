9a23a9a06821b2c69e3c9077465a9df1fb1b7117
Gilles Coomans <gilles.coomans@gmail.com>
2014-06-10 14:12:39 +0200

    add empty selectors unit

---
+++ units/selectors.js
@@ -0,0 +1,15 @@
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+define(["require","../deep"], function (require, deep) {
+    var unit = {
+        title:"deepjs/lib/selectors",
+        stopOnError:false,
+        setup:function(){},
+        tests : {
+           
+        }
+    };
+    return unit;
+});
+ 
