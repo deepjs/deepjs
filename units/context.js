c1842f2c9e34e7d0d6fcb116ddd448933d2d5abf
Gilles Coomans <gilles.coomans@gmail.com>
2014-08-24 16:47:48 +0200

    follow context refactoring

--- units/context.js
+++ units/context.js
@@ -15,74 +15,80 @@ define(["require","../deep", "../lib/unit"], function (require, deep, Unit) {
         stopOnError:false,
         setup:function(){},
         clean:function(){
-            deep.context = {};
+            deep.Promise.context = {};
         },
         tests : {
             setvar:function(){
-                deep.context = {};
+                deep.Promise.context = {};
                 var p1 = deep(9999)
+                .contextualise()
                 .toContext("test",1)
                 .done(function(){
-                    return deep.context.test;
+                    return deep.Promise.context.test;
                 })
                 .equal(1)
                 .done(function(){
                     return this.fromContext('test');
                 })
                 .equal(1);
-                var p2 = deep(deep.context).equal({});
+                var p2 = deep(deep.Promise.context).equal({});
                 return deep.all([p1,p2]);
             },
             setvar2:function(){
-                deep.context = {};
+                deep.Promise.context = {};
                 var p1 = deep(9999)
+                .contextualise()
                 .toContext("test",1)
                 .done(function(){
                     return deep(true)
+                    .contextualise()
                     .toContext("hello","world")
                     .done(function(){
-                        return deep.context;
+                        return deep.Promise.context;
                     })
                     .equal({test:1,hello:"world"});
                 })
                 .done(function(){
-                    return deep.context;
+                    return deep.Promise.context;
                 })
                 .equal({test:1});
-                var p2 = deep(deep.context).equal({});
+                var p2 = deep(deep.Promise.context).equal({});
                 return deep.all([p1,p2]);
             },
             delayed:function(){
-                deep.context = {};
+                deep.Promise.context = {};
                 var p1 = deep(9999)
+                .contextualise()
                 .toContext("test",1)
                 .delay(1)
                 .done(function(){
-                    return deep.context;
+                    return deep.Promise.context;
                 })
                 .equal({test:1});
 
                 var p2 = deep(9999)
+                .contextualise()
                 .toContext("test",2)
                 .delay(3)
                 .done(function(){
-                    return deep.context;
+                    return deep.Promise.context;
                 })
                 .equal({test:2});
 
                 var p3 = deep(9999)
+                .contextualise()
                 .toContext("test",3)
                 .delay(2)
                 .done(function(){
-                    return deep.context;
+                    return deep.Promise.context;
                 })
                 .equal({test:3});
 
-                var p4 = deep(deep.context).equal({});
+                var p4 = deep(deep.Promise.context).equal({});
                 return deep.all([p1,p2,p3,p4]);
             },
             delayed2:function(){
-                deep.context = {};
+                deep.Promise.context = {};
                 var ocm = deep.ocm({
                     a:{ hello:"world" },
                     b:{ _backgrounds:["this::../a"] }
